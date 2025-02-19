3883e02e9900c3989ff704e36ac016b94364744c
fractal-robot <udder-t-shirt-axis@duck.com>
2025-02-19 21:19:32 +0100

    05 part-2: done, HEHE

--- 05/part-2/main.zig
+++ 05/part-2/main.zig
@@ -65,27 +65,27 @@ fn parse_page_to_produce(
     try pages_to_produce.append(page_to_produce_numbers.items);
 }
 
-pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
+fn remove_at(
+    comptime T: type,
+    allocator: *const std.mem.Allocator,
+    slice: []T,
+    index: usize,
+) ![]T {
+    var new_slice = try allocator.alloc(T, slice.len - 1);
 
-    var ordering_rules_array = std.ArrayList([]isize).init(allocator);
-    var pages_to_produce_array = std.ArrayList([]isize).init(allocator);
-    try read_file_line_by_line(allocator, &ordering_rules_array, &pages_to_produce_array);
-    const ordering_rules = ordering_rules_array.items;
-    const pages_to_produce = pages_to_produce_array.items;
+    if (index > 0) @memcpy(new_slice[0..index], slice[0..index]);
+    if (index + 1 < slice.len) @memcpy(new_slice[index..], slice[index + 1 ..]);
 
-    std.debug.print("{any}\n", .{ordering_rules});
-    std.debug.print("{any}\n", .{pages_to_produce});
-
-    var sum: isize = 0;
-    for (pages_to_produce) |page| sum += get_value_from_page(page, ordering_rules);
-
-    std.debug.print("\n\nSum: {}\n", .{sum});
+    return new_slice;
 }
 
-pub fn get_value_from_page(page: []isize, ordering_rules: [][]isize) isize {
+// NOTE: Using a dictionnary would be a lot easier (and faster), but its enough
+// std-learning for the day.
+pub fn get_value_from_page(
+    allocator: *const std.mem.Allocator,
+    page: []isize,
+    ordering_rules: [][]isize,
+) !isize {
     var is_sequence_valid = true;
 
     var i: usize = 0;
@@ -98,7 +98,6 @@ pub fn get_value_from_page(page: []isize, ordering_rules: [][]isize) isize {
 
             for (ordering_rules) |ordering_rule| {
                 if (std.mem.eql(isize, &[_]isize{ page[i], number }, ordering_rule)) {
-                    std.debug.print("{any}\n", .{[_]isize{ page[i], number }});
                     is_following_number_valid = true;
                 }
             }
@@ -109,13 +108,47 @@ pub fn get_value_from_page(page: []isize, ordering_rules: [][]isize) isize {
         if (!is_number_valid) is_sequence_valid = false;
     }
 
-    const value: isize = blk: {
-        if (!is_sequence_valid) break :blk 0;
-        // if (page.len % 2 != 0) unreachable;
-        break :blk page[(page.len - 1) / 2];
-    };
+    if (is_sequence_valid) return 0;
 
-    std.debug.print("Local sum: {}\n", .{value});
+    var new_page = try allocator.alloc(isize, page.len);
 
-    return value;
+    i = 0;
+    while (i < page.len) : (i += 1) {
+        const number = page[i];
+        const other_numbers = try remove_at(isize, allocator, page, i);
+
+        var counter: usize = 0;
+        for (other_numbers) |other_number| {
+            var is_in_order = false;
+
+            for (ordering_rules) |ordering_rule| {
+                if (std.mem.eql(isize, &[_]isize{ number, other_number }, ordering_rule))
+                    is_in_order = true;
+            }
+
+            if (is_in_order)
+                counter += 1;
+        }
+
+        new_page[new_page.len - 1 - counter] = number;
+    }
+
+    return new_page[(page.len - 1) / 2];
+}
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    var ordering_rules_array = std.ArrayList([]isize).init(allocator);
+    var pages_to_produce_array = std.ArrayList([]isize).init(allocator);
+    try read_file_line_by_line(allocator, &ordering_rules_array, &pages_to_produce_array);
+    const ordering_rules = ordering_rules_array.items;
+    const pages_to_produce = pages_to_produce_array.items;
+
+    var sum: isize = 0;
+    for (pages_to_produce) |page| sum += try get_value_from_page(&allocator, page, ordering_rules);
+
+    std.debug.print("Sum: {}\n", .{sum});
 }
