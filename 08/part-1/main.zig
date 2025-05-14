c102450abda53ffb7bd28864097dcccebd9d5d83
Algorithmonaut <udder-t-shirt-axis@duck.com>
2025-05-14 20:00:28 +0200

    8 - part 1, almost done

--- 08/part-1/main.zig
+++ 08/part-1/main.zig
@@ -2,6 +2,25 @@ const std = @import("std");
 
 const print = std.debug.print;
 
+fn get_antenna_antinodes(locations: []const [2]i32) void {
+    for (locations) |base_location| {
+        for (locations) |other_location| {
+            if (std.mem.eql(i32, base_location[0..], other_location[0..]))
+                continue;
+
+            print("Base: {any}, other: {any}\n", .{ base_location, other_location });
+
+            const relative_row = other_location[0] - base_location[0];
+            const relative_col = other_location[1] - base_location[1];
+
+            const antinode_pos_row = base_location[0] - relative_row;
+            const antinode_pos_col = base_location[1] - relative_col;
+
+            print("There should be an antinode at {} {}\n", .{ antinode_pos_row, antinode_pos_col });
+        }
+    }
+}
+
 pub fn main() !void {
     var da = std.heap.DebugAllocator(.{}){};
     const allocator = da.allocator();
@@ -22,27 +41,29 @@ pub fn main() !void {
             else => false,
         };
 
+        if (is_antenna) {
+            print("Found antenna: {c} at {}-{} \n", .{ c, row, col });
+
+            const antenna = try antennas.getOrPut(c);
+
+            if (!antenna.found_existing) {
+                antenna.value_ptr.* = antenna_point.init(allocator);
+            }
+
+            try antenna.value_ptr.*.append(.{ row, col });
+        }
+
         col += 1;
         if (c == '\n') {
             row += 1;
             col = 0;
         }
-
-        if (!is_antenna) continue;
-        print("Found antenna: {c} at {}-{} \n", .{ c, row, col });
-
-        const antenna = try antennas.getOrPut(c);
-
-        if (!antenna.found_existing) {
-            antenna.value_ptr.* = antenna_point.init(allocator);
-        }
-
-        try antenna.value_ptr.*.append(.{ row, col });
     }
 
     var it = antennas.iterator();
 
     while (it.next()) |entry| {
         print("{c} -- {any}\n", .{ entry.key_ptr.*, entry.value_ptr.*.items });
+        get_antenna_antinodes(entry.value_ptr.items);
     }
 }
