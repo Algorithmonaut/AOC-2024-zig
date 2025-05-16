d90501e93fdb9ad73e8b0b3ae35eae699c6b51b3
Algorithmonaut <udder-t-shirt-axis@duck.com>
2025-05-16 11:58:04 +0200

    10 - part 2 - done

--- 09/part-2/main.zig
+++ 09/part-2/main.zig
@@ -1,3 +1,5 @@
+// NOTE: I think that I did really bad on this one
+
 const std = @import("std");
 const print = std.debug.print;
 
@@ -27,41 +29,69 @@ pub fn main() !void {
     var is_file = true;
     var id: i64 = 0;
 
-    var pos: usize = 0;
-    var new_pos: usize = 0;
-
-    for (data) |v| {
-        pos = new_pos;
-        new_pos = pos + v;
-
-        if (is_file) {
-            for (pos..new_pos) |i| {
-                array[i] = id;
-            }
-            id += 1;
-        } else {
-            for (pos..new_pos) |i| {
-                array[i] = -1;
-            }
-        }
-
-        is_file = !is_file;
-    }
-
     {
-        var i: usize = array.len - 1;
-        while (i > 0) : (i -= 1) {
-            if (array[i] != -1) {
-                for (array, 0..) |v, j| {
-                    if (v == -1) {
-                        array[j] = array[i];
-                        array[i] = -1;
-                        break;
-                    }
+        var pos: usize = 0;
+        var new_pos: usize = 0;
 
-                    if (j >= i) break;
+        for (data) |v| {
+            pos = new_pos;
+            new_pos = pos + v;
+
+            if (is_file) {
+                for (pos..new_pos) |i| {
+                    array[i] = id;
+                }
+                id += 1;
+            } else {
+                for (pos..new_pos) |i| {
+                    array[i] = -1;
                 }
             }
+
+            is_file = !is_file;
+        }
+    }
+
+    var pos = array.len - 1;
+    while (pos > 0) {
+        if (array[pos] == -1) {
+            pos -= 1;
+            continue;
+        }
+
+        const start = pos;
+        const char = array[start];
+
+        var i: usize = 0;
+        while (pos - i > 0 and array[start - i] == char) : (i += 1) continue;
+
+        pos -= i;
+
+        const length: usize = start - pos;
+
+        var it = std.mem.window(i64, array, length, 1);
+        var pat_buf: [256]i64 = undefined;
+        const pat: []const i64 = pat_buf[0..length];
+        for (pat_buf[0..length]) |*slot| slot.* = -1;
+
+        var j: usize = 0;
+        while (it.next()) |window| {
+            if (std.mem.eql(i64, window, pat)) {
+                break;
+            }
+
+            j += 1;
+        }
+
+        if (j + length > array.len) continue;
+        if (j > pos) continue;
+
+        for (0..length) |idx| {
+            array[j + idx] = char;
+        }
+
+        for (pos..pos + length) |idx| {
+            array[idx + 1] = -1;
         }
     }
 
